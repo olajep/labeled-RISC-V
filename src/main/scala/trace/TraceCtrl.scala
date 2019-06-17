011ec967a45e62e4ed2bf3f7ea1ddf499f15e658
Ola Jeppsson <ola.jeppsson@gmail.com>
2019-06-17 18:10:28 -0400

    trace: TraceCtrl: Cont'd support for more than one harts
    Work in progress...
    
    NB: Register map changed again.
    We have enough buffer status bits for
    4096 harts.
    And enough buffer address space for 176 harts inside a 4096 bytes page.
    But there's nothing above the buffer address space so this can be
    extended.

--- src/main/scala/trace/TraceCtrl.scala
+++ src/main/scala/trace/TraceCtrl.scala
@@ -49,39 +49,46 @@ trait TraceCtrlModule extends HasRegMap
   val buf0_full_clear = Wire(init=Bool(false))
   val buf1_full = RegInit(UInt(0, width = 1))
   val buf1_full_clear = Wire(init=Bool(false))
-  val (buf0_addr, buf0_addr_desc) =
-    DescribedReg(UInt(addrWidth.W), "buf0_addr", "Base address of trace buffer 0. Must have buf_mask alignment.",
-      reset=Some(0.U(addrWidth.W)), volatile=true)
   val (buf_mask, buf_mask_desc) =
-    DescribedReg(UInt(addrWidth.W), "buf_mask", "Size of trace buffer 0 minus 1.",
+    DescribedReg(UInt(addrWidth.W), "buf_mask", "Size of trace buffer minus 1.",
       reset=Some(0.U(addrWidth.W)), volatile=true)
-  val (buf1_addr, buf1_addr_desc) =
-    DescribedReg(UInt(addrWidth.W), "buf1_addr", "Base address of trace buffer 0. Must have buf_mask alignment.",
-      reset=Some(0.U(addrWidth.W)), volatile=true)
+  val buf0_addr = Reg(UInt(64.W))
+  val buf1_addr = Reg(UInt(64.W))
 
   // Wire up inputs
   buf0_full := io.harts(0).in.buf0_full.asUInt
   buf1_full := io.harts(0).in.buf1_full.asUInt
 
+  // Connect interrupts
   val any_buf_full : Bool =
     io.harts.exists { a => a.in.buf0_full || a.in.buf1_full }
-
-  // Connect interrupts
   interrupts := Vec.tabulate(1)
     { _ => (enable & irq_en & any_buf_full).toBool }
 
   def reg(r: UInt, gn: String, d: RegFieldDesc) =
     RegFieldGroup(gn, None, RegField.bytes(r, (r.getWidth + 7)/8, Some(d)))
-  regmap(
-    0x00 -> Seq( /* config */
+
+  def status_addr_base = 0x100
+  def buf_addr_base    = 0x500
+
+  val config_reg_fields = Seq(
+    0x00 -> Seq(
       RegField(1, enable,
-        RegFieldDesc("enable", "Enable: Set to one to enable tracing.", reset = Some(0))),
+        RegFieldDesc("enable",
+                     "Enable: Set to one to enable tracing.",
+                     reset = Some(0))),
       RegField(1, irq_en,
-        RegFieldDesc("irq_en", "Trace buffer full interrupt.", reset = Some(0))),
+        RegFieldDesc("irq_en",
+                     "Trace buffer full interrupt.",
+                     reset = Some(0))),
       RegField(1, ignore_illegal_insn,
-        RegFieldDesc("ignore_illegal_insn", "Don't trace illegal instructions.", reset = Some(0)))),
-    0x08 -> reg(buf_mask, "buf_mask", buf_mask_desc),
-    0x10 -> Seq( /* status */
+        RegFieldDesc("ignore_illegal_insn",
+                     "Don't trace illegal instructions.",
+                     reset = Some(0)))),
+    0x08 -> reg(buf_mask, "buf_mask", buf_mask_desc))
+
+  val status_reg_fields = Seq(
+    status_addr_base -> Seq(
       RegField(1,
         RegReadFn { _ => (Bool(true), buf0_full) },
         RegWriteFn { (valid, clear) =>
@@ -97,10 +104,24 @@ trait TraceCtrlModule extends HasRegMap
           Bool(true)
         },
         RegFieldDesc("buf1_full", "Trace buffer1 full.",
-                     reset = Some(0), volatile=true))),
-    0x18 -> reg(buf0_addr, "buf0_addr", buf0_addr_desc),
-    0x20 -> reg(buf1_addr, "buf1_addr", buf1_addr_desc)
-  )
+                     reset = Some(0), volatile=true))))
+
+  def buf_addr_reg_desc(h: Int, i: Int) =
+    RegFieldDesc(
+      name      = s"hart_${h}_buf${i}_addr",
+      desc      = s"Trace buffer ${i} address for hart ${h}",
+      group     = Some("buf_addr"),
+      groupDesc = Some("Trace buffer address."),
+      reset     = Some(BigInt(0)))
+
+  val buf_addr_reg_fields = Seq.tabulate(params.nharts) { h =>
+    buf_addr_base + h * 16 + 0 -> reg(buf0_addr, s"buf0_addr_hart${i}",
+                                      buf_addr_reg_desc(h, 0))
+    buf_addr_base + h * 16 + 8 -> reg(buf1_addr, s"buf1_addr_hart${i}",
+                                      buf_addr_reg_desc(h, 1))
+  }
+
+  regmap((config_reg_fields ++ status_reg_fields ++ buf_addr_reg_fields):_*)
 
   // Pipeline outputs
   val enable_reg              = RegNext(enable.toBool)
